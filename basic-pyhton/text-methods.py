31780968161fb91db91ce6e494536dcb10d5d154
Ornella Gugliotta <arielgugliotta89@gmail.com>
2021-01-30 08:40:51 -0300

    Metodos de texto en python

---
+++ basic-pyhton/text-methods.py
@@ -0,0 +1,8 @@
+nombre = input('Cual es tu nombre?: ')
+print('Upper ' + nombre.upper())
+print('Proper ' + nombre.capitalize())
+print('Lower ' + nombre.lower())
+print('Replace ' + nombre.replace('o', 'a'))
+print('Length ' + str(len(nombre)))
+
+print('Trim ' + nombre.strip()) 
